5aa84e3cb03cd90cf12d411e198ac52ca2270666
axewhyzed <coding0234@gmail.com>
2023-06-14 21:18:42 +0530

    Updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,56 @@
-import React, { useState } from 'react'
-import Notes from './components/Notes'
+import React, { useState } from 'react';
+import Notes from './components/Notes';
 
 const App = () => {
-  const [inputList, setInputList] = useState("");
-
-  const [Items, setItems] = useState([])
+  const [inputList, setInputList] = useState('');
+  const [Items, setItems] = useState([]);
 
   const itemEvent = (event) => {
     setInputList(event.target.value);
-  }
+  };
 
   const listOfItems = () => {
-    setItems((oldItems) => {
-      return [...oldItems, inputList];
-    })
-    setInputList('');
+    if (inputList.trim() !== '') {
+      setItems((oldItems) => [...oldItems, inputList]);
+      setInputList('');
+    }
   };
 
   const deleteItems = (id) => {
-    setItems((oldItems) => {
-      return oldItems.filter((arrEle, eleId) => {
-        return eleId !== id;
-      })
-    })
-    console.log("bork");
+    setItems((oldItems) => oldItems.filter((_, index) => index !== id));
   };
 
   return (
-    <div className='main_div'>
-      <div className='center_div' >
-        <br />
-        <h1>Note it !</h1>
-        <br />
-        <input type="text"
-          placeholder='Add a new note'
-          value={inputList}
-          onChange={itemEvent} />
-        <button onClick={listOfItems}> + </button>
-
+    <div className="main_div">
+      <div className="center_div">
+        <h1>Note It!</h1>
+        <div className="input_container">
+          <input
+            type="text"
+            placeholder="Add a new note"
+            value={inputList}
+            onChange={itemEvent}
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                listOfItems();
+              }
+            }}
+          />
+          <button onClick={listOfItems}>+</button>
+        </div>
         <ol>
-          {Items.map((itemval, itemId) => {
-            return <Notes
+          {Items.map((itemval, itemId) => (
+            <Notes
               key={itemId}
               id={itemId}
               text={itemval}
               onSelect={deleteItems}
             />
-          })}
+          ))}
         </ol>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
